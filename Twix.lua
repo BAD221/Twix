cc586ab5cd2b3dfffa731aad3ed0cfe01706f971
HIEDAR ZEAD <51862485+BAD221@users.noreply.github.com>
2021-11-18 04:06:24 +0300

    Update Twix.lua

--- Twix.lua
+++ Twix.lua
@@ -8137,7 +8137,25 @@ Dev_Hid(msg.chat_id_, msg.id_, 1, t, 1, 'html')
 end
 end
 --     Source Twix     --
-if text == "غنيلي" and ChCheck(msg) then
+if text == 'تفعيل غنيلي' and CoSu(msg) then   
+if database:get(bot_id..'sing:for:me'..msg.chat_id_) then
+Text = ' ⌔︙ تم تفعيل امر غنيلي الان ارسل غنيلي'
+database:del(bot_id..'sing:for:me'..msg.chat_id_)  
+else
+Text = ' ⌔︙ بالتاكيد تم تفعيل امر غنيلي تستطيع ارسال غنيلي'
+end
+send(msg.chat_id_, msg.id_,Text) 
+end
+if text == 'تعطيل غنيلي' and CoSu(msg) then  
+if not database:get(bot_id..'sing:for:me'..msg.chat_id_) then
+database:set(bot_id..'sing:for:me'..msg.chat_id_,true)  
+Text = '\n ⌔︙ تم تعطيل امر غنيلي'
+else
+Text = '\n ⌔︙ بالتاكيد تم تعطيل امر غنيلي'
+end
+send(msg.chat_id_, msg.id_,Text) 
+end
+if text == "غنيلي" and CoSu(msg) then
 data,res = https.request('https://apiabs.ml/Audios.php')
 if res == 200 then
 Audios = json:decode(data)
