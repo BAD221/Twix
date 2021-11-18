610d3367b91714e254a95042aa1ae769be8cdfa2
HIEDAR ZEAD <51862485+BAD221@users.noreply.github.com>
2021-11-18 04:16:28 +0300

    Update Twix.lua

--- Twix.lua
+++ Twix.lua
@@ -8164,7 +8164,7 @@ if audios.Info == true then
 local Text ='⌔︙تم اختيار المقطع الصوتي لك'
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text = '⌔︙ Twix TEAM .',url="t.me/x_xxax}},
+{{text = '⌔︙ Twix TEAM .',url="t.me/x_xxax"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..TokenBot..'/sendVoice?chat_id=' .. msg.chat_id_ .. '&voice='..URL.escape(Audios.info)..'&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
